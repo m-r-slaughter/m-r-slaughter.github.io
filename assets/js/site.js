921c65b523002e79a3d26b32b463b2b6f995ffc9
m-r-slaughter <monica.rene.slaughter@gmail.com>
2024-12-10 14:54:29 -0700

    Remove subtext

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,7 @@
-new TypeIt('#descriptions', {
-    strings: ["Craftsperson.", "Gamer.", "Nerd.", "Not 3 Dogs in a trenchcoat.", "Mentor."],
-    speed: 200,
-    breakLines: false,
-    waitUntilVisible: true,
-    loop: true,
-}).go();
+// new TypeIt('#descriptions', {
+//     strings: ["Craftsperson.", "Gamer.", "Nerd.", "Not 3 Dogs in a trenchcoat.", "Mentor."],
+//     speed: 200,
+//     breakLines: false,
+//     waitUntilVisible: true,
+//     loop: true,
+// }).go();
