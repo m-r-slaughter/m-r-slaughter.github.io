9f42f0de4b01f221f1d937350bf0429088d9b183
m-r-slaughter <monica.rene.slaughter@gmail.com>
2025-03-06 14:38:31 -0700

    Remove unused code and move js

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function getCurrentYear(){
-  const currentYear = new Date().getFullYear();
-  return currentYear;
-}
-
-let dateSpan = document.querySelector('.date');
-
-dateSpan.innerHTML = getCurrentYear;
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,8 @@
-// new TypeIt('#descriptions', {
-//     strings: ["Craftsperson.", "Gamer.", "Nerd.", "Not 3 Dogs in a trenchcoat.", "Mentor."],
-//     speed: 200,
-//     breakLines: false,
-//     waitUntilVisible: true,
-//     loop: true,
-// }).go();
+function getCurrentYear(){
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+  }
+  
+  let dateSpan = document.querySelector('.date');
+  
+  dateSpan.innerHTML = getCurrentYear;
